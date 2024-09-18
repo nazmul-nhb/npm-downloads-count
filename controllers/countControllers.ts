9f09f1a7517cfc79913a7ed0653e9c68fdddfa84
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-18 23:03:00 +0600

    project initialized

---
+++ controllers/countControllers.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+const getDownloadsCount = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
